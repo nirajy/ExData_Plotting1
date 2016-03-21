6d1ebd1677871293402db251295138fc8d9c2dd9
Niraj Yadav <nirajy.yadav@gmail.com>
2016-03-20 20:42:28 -0400

    Adding Png and R files

---
+++ plot3.R
@@ -0,0 +1,26 @@
+library(sqldf) ##Load sqldf library
+
+## read 5 rows to get Column classes
+input5 <- read.table('household_power_consumption.txt',header=TRUE,na.strings = "?", nrows = 5, sep=';')
+classes <- sapply(input5,class)
+## Read House Hole Power Consumption file only for 1/2/2007 and 2/2/2007
+input <- read.csv.sql('household_power_consumption.txt',sep=';',colClasses = classes, header = TRUE, eol = '\n', sql = " select * from file where Date = '1/2/2007' or Date = '2/2/2007' ")
+## Add new Columne DateTime
+input <- cbind(input,DateTime="")
+## Merge Date and Time with Space as seperator
+input$DateTime <- paste(input$Date,input$Time,sep=" ")
+
+##Convert into Datetime Using strptime
+input$DateTime <- strptime(input$DateTime, format='%d/%m/%Y %H:%M:%S')
+
+#Plot 
+png("plot3.png",width=480,height=480,unit="px")
+
+
+plot(input$DateTime,input$Sub_metering_1,xlab="",ylab="Energy sub metering",type="l")
+lines(input$DateTime,input$Sub_metering_2,type="l",col="red")
+lines(input$DateTime,input$Sub_metering_3,type="l",col="blue")
+legend("topright", c("sub_metering_1","sub_metering_2","sub_metering_3"), col=c("black","red","blue"),lwd=1)
+
+#Save Into Plot1.png
+dev.off ()
---
+++ plot4.R
@@ -0,0 +1,31 @@
+library(sqldf) ##Load sqldf library
+
+## read 5 rows to get Column classes
+input5 <- read.table('household_power_consumption.txt',header=TRUE,na.strings = "?", nrows = 5, sep=';')
+classes <- sapply(input5,class)
+## Read House Hole Power Consumption file only for 1/2/2007 and 2/2/2007
+input <- read.csv.sql('household_power_consumption.txt',sep=';',colClasses = classes, header = TRUE, eol = '\n', sql = " select * from file where Date = '1/2/2007' or Date = '2/2/2007' ")
+## Add new Columne DateTime
+input <- cbind(input,DateTime="")
+## Merge Date and Time with Space as seperator
+input$DateTime <- paste(input$Date,input$Time,sep=" ")
+
+##Convert into Datetime Using strptime
+input$DateTime <- strptime(input$DateTime, format='%d/%m/%Y %H:%M:%S')
+
+#Plot 
+png("plot4.png",width=480,height=480,unit="px")
+
+par(mfrow = c (2,2))
+plot(input$DateTime,input$Global_active_power,type="l",xlab="",ylab="Global Active Power")
+plot(input$DateTime,input$Voltage,type="l",xlab="datetime",ylab="voltage")
+
+plot(input$DateTime,input$Sub_metering_1,xlab="",ylab="Energy sub metering",type="l")
+lines(input$DateTime,input$Sub_metering_2,type="l",col="red")
+lines(input$DateTime,input$Sub_metering_3,type="l",col="blue")
+legend("topright", c("sub_metering_1","sub_metering_2","sub_metering_3"), col=c("black","red","blue"),lwd=1, bty="n")
+
+plot(input$DateTime,input$Global_reactive_power,xlab="datetime",ylab="Global_reactive_power",type="l")
+
+#Save Into Plot1.png
+dev.off ()
